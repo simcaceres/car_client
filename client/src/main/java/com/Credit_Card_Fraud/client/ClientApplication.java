9c9b66c33d401d9a60f6d36aac6077f64a2566d1
VA-Alumno <sim.caceres@duocuc.cl>
2026-06-17 17:33:16 -0400

    cambios echo

--- client/src/main/java/com/Credit_Card_Fraud/client/ClientApplication.java
+++ client/src/main/java/com/Credit_Card_Fraud/client/ClientApplication.java
@@ -2,23 +2,26 @@ package com.Credit_Card_Fraud.client;
 
 import com.Credit_Card_Fraud.client.controller.TransaccionController;
 import com.Credit_Card_Fraud.client.service.TransaccionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ClientApplication {
+public class ClientApplication implements CommandLineRunner {
+
+    @Autowired
+    private TransaccionService service;
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner init(TransaccionService transaccionService){
-        return  args -> {
-            transaccionService.iniciarPipeline();
-        };
+    @Override
+    public void run(String... args) throws Exception {
+        // 🌟 EL CAMBIO: Ponle la ruta adentro de los paréntesis para que no vaya vacío
+        service.iniciarPipeline("data/raw/datos_fraude.csv");
     }
 
 }
